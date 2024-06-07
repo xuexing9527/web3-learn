5c2c12929f03cbd79b59f92c2f8d57a6a390e3b5
fn <xuexing9527@163.com>
2024-06-07 19:11:56 +0800

    pull

--- web3-next/app/vote/components/Section/index.tsx
+++ web3-next/app/vote/components/Section/index.tsx
@@ -11,7 +11,8 @@ declare global {
 export default function Section () {
     useEffect(() => {
         // var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-        var web3 = new Web3('http://localhost:7545');
+        // var web3 = new Web3('http://localhost:7545');
+        var web3 = new Web3('http://localhost:8551');
         console.log(web3.eth.providers)
         web3.eth.getCoinbase().then(console.log);
         web3.eth.getAccounts().then(console.log);
